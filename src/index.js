3d2672661ff8ce625839dbb63cdf41c4fee3821d
Nam Eungyeong <nek1717@naver.com>
2022-01-11 23:36:54 +0900

    깃허브 페이지 경로 수정

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import './globalStyles.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
